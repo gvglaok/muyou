44cb92f77bb9458f2957276694a028985837d242
ufowiner <ufowiner@sina.com>
2017-09-02 01:31:03 +0800

    doing

--- resources/views/bcon/dashboard.blade.php
+++ resources/views/bcon/dashboard.blade.php
@@ -19,5 +19,21 @@
 			组队发布 ：999
 		</div>
 	</div>
+	<div class="form-group">
+		<label for="textarea" class="col-sm-2 control-label">:</label>
+		<div class="col-sm-10">
+			<textarea name="" id="textarea" class="form-control" rows="3" required="required">
+				
+			- 游戏新闻
+			- 出游建议
+
+			- 队友招募
+			- 招募打赏
+			- 奖金
+			- 平台聊天留言
+
+			</textarea>
+		</div>
+	</div>
 	
 @endsection
