e2e0712dc3f20520f72c12f367da9866c2c2b716
ufowiner <ufowiner@sina.com>
2017-09-07 22:41:07 +0800

    test

--- resources/views/bcon/URP.blade.php
+++ resources/views/bcon/URP.blade.php
@@ -51,7 +51,7 @@
 			<div class="modal-content">
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					<h4 class="modal-title">添加用户</h4>
+					<h4 class="modal-title">添加用户-------</h4>
 				</div>
 				<div class="modal-body">
 					
